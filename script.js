670184b1a6d9c33aae90d252385cf5ca5f23adc0
oluwatimileyin Ashaye <111620066+timiashaye@users.noreply.github.com>
2023-03-20 20:18:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = function () {
-        alert("Hello , Am Lil Tims the single lonely boy 😂😂")
+        alert("Hello , Am Lil Tims and i have girl")
     }
 });
