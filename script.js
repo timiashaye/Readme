fa8d7dcc7721d0bf192d3c3464ee087985eef30b
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-03-20 13:28:11 -0800

    added a little javascript functionality to the button

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').onclick = function () {
+        alert("Hello , Am Lil Tims the single lonely boy 😂😂")
+    }
+});
