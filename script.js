efae925b5ec8c8ab092c9e0bd6281a6e4bf81506
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-03-20 22:38:26 -0800

    i added a select function to pick a choice

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = function () {
         alert("Hello , Am Lil Tims and i have girl")
     }
+
+    document.querySelector('select').onchange = function() {
+        let name = this.value
+        alert(`Eiii Timmy so you like  ${name} `)
+
+    }
 });
